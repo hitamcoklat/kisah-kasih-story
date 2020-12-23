589195c194085fecb2ba68db143b5c3af8d011f6
tian <septiandwianugrah@gmail.com>
2020-12-23 11:49:45 +0700

    update info

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -23,18 +23,15 @@
         <div class="border-0 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
           <!--header-->
           <div class="flex items-start justify-between p-2 border-b border-solid border-gray-300 rounded-t">
-            <h3 class="text-xl font-semibold">Menu</h3>
+            <h3 class="text-3xl text-black py-5 pl-5 font-semibold">Tentang</h3>
           </div>
           <!--body-->
-          <div class="p-2 flex-auto">
-            <button class="bg-grey-light w-full hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
-                <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                <span>Download</span>
-            </button>              
+          <div class="p-2 flex-auto py-10 px-10">
+            <p class="text-black text-justify text-base"><strong>Kisah</strong>. Merupakan sebuah platform baca, berasal dari kumpulan2 artikel/tulisan yang ada di internet dan dikumpulkan dengan metoda scrapping. Mohon berkomentar yang bijak untuk mencerminkan netizen yang budiman, baik hati dan rajin menabung. :)</p>      
           </div>
           <!--footer-->
           <div class="flex items-center justify-end p-2 border-t border-solid border-gray-300 rounded-b">
-            <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" @click="showModal = false">
+            <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 w-full" type="button" style="transition: all .15s ease" @click="showModal = false">
               Close
             </button>
           </div>
